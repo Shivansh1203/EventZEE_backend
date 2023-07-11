38ca3b2c035bcf93e833ada01a4f61674da4df18
Shivansh Rastogi <88771326+Shivansh1203@users.noreply.github.com>
2023-07-11 16:37:21 +0530

    something

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://shivansh:shivansh@123@cluster0.fbadq.mongodb.net/?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://shivansh:shivansh@123@cluster0.fbadq.mongodb.net/studentRegn?retryWrites=true&w=majority");
 
 
 
